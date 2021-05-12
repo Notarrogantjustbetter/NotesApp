4bbe36ec9db7eef780205e7f49bb0c3532e2f0b3
Nikolay Nikolaev <66773789+Notarrogantjustbetter@users.noreply.github.com>
2021-05-12 11:13:09 +0300

    Update template.go

--- utils/template.go
+++ utils/template.go
@@ -5,13 +5,12 @@ import (
 	"net/http"
 )
 
-
 var tmpl *template.Template
 
 func LoadTemplate() {
 	tmpl = template.Must(template.ParseGlob("templates/*.html"))
 }
 
-func ExecuteTemplate(w http.ResponseWriter, templ string, data interface{}) {
+func ExecuteTemplate(w http.ResponseWriter, templ string, data interface{}){
 	tmpl.ExecuteTemplate(w, templ, data)
 }
